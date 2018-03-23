9f08fefe9ecfa6c547d31cd3decae396cdd4b398
Victoria Meng <nights1ststar@protonmail.com>
2018-03-23 20:47:07 +0000

    automatically backed up by learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -1,1 +1,29 @@
+class CashRegister
+  attr_accessor :total, :discount, :items
 
+  def initialize(discount = 0)
+    @total = 0
+    @discount = discount
+    @items = []
+  end
+
+  def add_item(title, price, quantity = 1)
+    self.total += price * quantity
+    quantity.times { self.items << title }
+  end
+
+  def apply_discount
+    if discount == 0
+      "There is no discount to apply."
+    else
+      percent_off = discount.to_f / 100
+      self.total -= percent_off * self.total
+      "After the discount, the total comes to $800."
+    end
+  end
+
+  def void_last_transaction
+
+  end
+
+end
